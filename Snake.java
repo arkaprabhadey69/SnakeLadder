b52afbcd38605d51b6856c22b4e83aa621e029f6
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-22 18:02:42 +0530

    [Arka]Added:Options

--- Snake.java
+++ Snake.java
@@ -1,21 +1,48 @@
 public class Snake
 {
-	
+	 static final int NO_PLAY=0;
+	 static final int LADDER=1;
+	 static final int SNAKE=2;
 	public static void main(String[] args) {
 
 		double start_position=0;
 		double new_position=0;
 		
 
-		while(new_position<8)
+		while(new_position<100)
 	{
 		
 		double dice=(Math.floor(Math.random()*10)%6)+1;
-		new_position=new_position+dice;
-		System.out.println("New position is:" +new_position);
+		int options=(int)Math.floor(Math.random()*10)%3;
+		//System.out.println(dice);
+		//double new_position=0;
+
+		
+		System.out.println("Dice number:"+dice);
+		System.out.println("Option"+ options);
+		switch(options)
+		{
+			case NO_PLAY:
+			new_position=new_position;
+			
+			break;
+			case LADDER:
+			new_position=new_position+dice;
+			break;
+			case SNAKE:
+			new_position=new_position -dice;
+			if(new_position<0)
+			{
+				new_position=start_position;
+				System.out.println("Starts from starting position");
+			}
+			break;
+			 default:
+			break;
+		}
 		
 	}
-		
+		System.out.println("Final position after diceroll is: "+ new_position);
 		
 
 
