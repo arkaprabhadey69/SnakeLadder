1206f17f6b807ead559dedf216ce05cb48d152da
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-22 17:57:21 +0530

    [Arka]Added:Rolling dice

--- Snake.java
+++ Snake.java
@@ -1,15 +1,34 @@
-public class SnakeUC1
+public class Snake
 {
-	 
+	
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Snake Ladder Match");
-
 		double start_position=0;
+		double new_position=0;
 		
 
-	
+		while(new_position<8)
+	{
+		
+		double dice=(Math.floor(Math.random()*10)%6)+1;
+		new_position=new_position+dice;
+		System.out.println("New position is:" +new_position);
+		
+	}
+		
+		
+
+
+
+
+
+		}
 
 		
 }
-}
+
+
+
+
+
+
