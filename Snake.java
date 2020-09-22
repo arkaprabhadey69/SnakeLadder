1bf5342abc56684ce4e1f5654abfd5906e166cdd
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-22 18:20:55 +0530

    [Arka]Refactored: Two players

--- Snake.java
+++ Snake.java
@@ -3,16 +3,59 @@ public class Snake
 	 static final int NO_PLAY=0;
 	 static final int LADDER=1;
 	 static final int SNAKE=2;
+	 static int no_of_times=0;
 	public static void main(String[] args) {
 
-		double start_position=0;
-		double new_position=0;
-		int no_of_times=0;
+		double start_position1=0;
+		double start_position2=0;
+		double new_position1=0;
+		double new_position2=0;
+		int ch=(int)Math.floor(Math.random()*10)%2;
 
-		while(new_position!=100)
+		while(new_position1!=100||new_position2!=100)
+{
+		
+		if(ch==0)
 	{
-		no_of_times++;
-		double dice=(Math.floor(Math.random()*10)%6)+1;
+		System.out.println("player1");
+
+		new_position1=diceroll(new_position1,start_position1);
+		System.out.println("New Position for P1 is: "+new_position1);
+
+		if(new_position1==100)
+		{
+			System.out.println("Player 1 won");
+		}
+		ch=1;
+	}
+	else
+	{
+		System.out.println("Player2");
+
+		new_position2=diceroll(new_position2,start_position2);
+		System.out.println("New position for P2 is: "+new_position2);
+		
+		if(new_position2==100)
+		{
+			System.out.println("Player 2 won");
+		}
+		ch=0;
+
+		
+	}
+}
+	System.out.println("Dice was rolled : "+no_of_times+" times");
+		
+
+
+
+
+
+ }
+ public static double diceroll(double new_position, double start_position)
+		{
+			no_of_times++;
+			double dice=(Math.floor(Math.random()*10)%6)+1;
 		int options=(int)Math.floor(Math.random()*10)%3;
 		//System.out.println(dice);
 		//double new_position=0;
@@ -33,6 +76,7 @@ public class Snake
 				new_position=new_position-dice;
 				System.out.println("Stays in same position");
 			}
+			diceroll(new_position,start_position);
 			break;
 			case SNAKE:
 			new_position=new_position -dice;
@@ -45,15 +89,10 @@ public class Snake
 			 default:
 			break;
 		}
-		System.out.println("New Position after dice roll: "+new_position);
-	}
-		System.out.println(new_position);
-		System.out.println("Dice was rolled "+no_of_times+" times");
+		//System.out.println("New Position after dice roll: "+new_position);
 
 
-
-
-
+		return new_position;
 		}
 
 		
