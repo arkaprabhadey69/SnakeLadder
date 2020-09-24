c56194c37301aed4727c2ea7f75b9ecd9556762e
arkaprabhadey69 <47019998+arkaprabhadey69@users.noreply.github.com>
2020-09-24 08:55:56 +0530

    [Arka]refactored: Added Dice

--- Snake.java
+++ Snake.java
@@ -6,15 +6,7 @@ public class Snake
 		double start_position=0;
 		double new_position=0;
 		
-
-		while(new_position<8)
-	{
-		
 		double dice=(Math.floor(Math.random()*10)%6)+1;
-		new_position=new_position+dice;
-		System.out.println("New position is:" +new_position);
-		
-	}
 		
 		
 
